31ce1c7e03fb0d31b6df8bddee7770c238deb3e8
pedrokkjzk <167801838+Pedrohenrc@users.noreply.github.com>
2024-07-14 11:14:10 -0300

    Update primos.py

--- primos.py
+++ primos.py
@@ -2,30 +2,23 @@ import math
 def checar(p):
         while True:
                 try:
-                    numero = int(input("Por favor, insira um número maior que 1: "))
+                    numero = int(input(f"Por favor, insira um número maior que 1: "))
                     if numero > 1:
                         return numero
                     else:
                         print("Erro: o número deve ser maior que 1. Tente novamente.")
                 except ValueError:
-                    print("Entrada inválida. insira outro número.")
+                    print("Entrada inválida. insira outro número.")
 p = 1
 n = checar(p)
-def primo(n):
-    if n == 2 or n == 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    for i in range(5, int(math.sqrt(n)) + 1, 2):
-        if n % i == 0:
-            return False
-    return True
+numerosp = []
 
-def numerosp(n):
-    listap = []
-    for x in range (1,(n + 1):
-        if primo(x):
-            listap += [x]
-    return listap
-        
-print (f"Números primos até {n}: {numerosp(n)}")
+for x in range (2, n + 1):
+    div = 0
+    for i in range(1, n + 1):
+        if x % i == 0:
+            div += 1
+    if div == 2:
+        numerosp += [x]
+             
+print (f'Estes são todos os numeros primos entre 0 e {n}: {numerosp} ')
